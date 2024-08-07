809b473d590fa82f116e1c65d611449cde77c6e4
SatoshiMorinaga <stsmorinaga@gmail.com>
2024-08-08 02:26:29 +0900

    nginx+css適用テスト5

--- myproject/myproject/settings.py
+++ myproject/myproject/settings.py
@@ -137,7 +137,6 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')] 
 
