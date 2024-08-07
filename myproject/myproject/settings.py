1de94eca178b1c0d04994f3daff447ffc7317d68
SatoshiMorinaga <stsmorinaga@gmail.com>
2024-08-08 01:48:34 +0900

    nginx+css適用テスト

--- myproject/myproject/settings.py
+++ myproject/myproject/settings.py
@@ -153,7 +153,8 @@ if not DEBUG:
     AWS_S3_OBJECT_PARAMETERS = {'CacheControl': 'max-age=86400',}
     AWS_LOCATION = 'static'
     AWS_DEFAULT_ACL = None
-    STATIC_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/{AWS_LOCATION}/'
+    #STATIC_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/{AWS_LOCATION}/'
+    STATIC_URL = '/static/'
     STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 
 # ログイン認証
