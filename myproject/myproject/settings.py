d144a29bba84143ec003c40bc624f3a83d8dfa95
SatoshiMorinaga <stsmorinaga@gmail.com>
2024-08-08 02:00:34 +0900

    nginx+css適用テスト3

--- myproject/myproject/settings.py
+++ myproject/myproject/settings.py
@@ -155,6 +155,7 @@ if not DEBUG:
     AWS_DEFAULT_ACL = None
     #STATIC_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/{AWS_LOCATION}/'
     STATIC_URL = '/static/'
+    STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')] 
     #STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 
 # ログイン認証
