7fae3fe946e13228e81764f0c9b42fc27a7d11e7
SatoshiMorinaga <stsmorinaga@gmail.com>
2024-08-07 13:13:07 +0900

    collectstatic対応

--- myproject/myproject/settings.py
+++ myproject/myproject/settings.py
@@ -134,10 +134,10 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
-STATIC_URL = 'static/'
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 if DEBUG:
+    STATIC_URL = '/static/'
     # 画像関連の設定
     MEDIA_URL = '/media/'
     MEDIA_ROOT = BASE_DIR / 'media_local'
