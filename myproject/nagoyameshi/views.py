90127c7c10c8b9a9bcb34173a8e4f3cf8eb42057
SatoshiMorinaga <stsmorinaga@gmail.com>
2024-09-17 18:33:50 +0900

    テスト会員のプロフィール変更禁止

--- myproject/nagoyameshi/views.py
+++ myproject/nagoyameshi/views.py
@@ -595,7 +595,7 @@ class UserView(OnlyMyUserInformationMixin, LoginRequiredMixin, DetailView):
     
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        context["user_id"] = self.request.user.pk
+        context["user"] = self.request.user
         return context
 
 class UserUpdateView(OnlyMyUserInformationMixin, UpdateView):
